5e49abcfd6560c34e84f80d22d3e144cc8c1c02a
Chia Wai Eugene <20041877@myrp.edu.sg>
2021-05-18 13:35:25 +0800

    done

---
+++ app/src/main/java/sg/edu/rp/c346/id20041877/demotimeanddateapp/MainActivity.java
@@ -0,0 +1,73 @@
+package sg.edu.rp.c346.id20041877.demotimeanddateapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DatePicker dp;
+        TimePicker tp;
+        Button btnDisplayDate;
+        Button btnDisplayTime;
+        TextView tvDisplay;
+        Button btnReset;
+
+        dp = findViewById(R.id.datePicker);
+        tp = findViewById(R.id.timePicker);
+        btnDisplayDate = findViewById(R.id.buttonDisplayDate);
+        btnDisplayTime = findViewById(R.id.buttonDisplayTime);
+        tvDisplay = findViewById(R.id.textViewDisplay);
+        btnReset = findViewById(R.id.buttonReset);
+
+        btnDisplayTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int hour = tp.getHour();
+                int min = tp.getMinute();
+
+                if (min  <11) {
+                    tvDisplay.setText("Time is " + hour + ":0" + min);
+                }
+                else {
+                    tvDisplay.setText("Time is " + hour + ":" + min);
+                }
+            }
+        });
+
+
+        btnDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int day = dp.getDayOfMonth();
+                int month = dp.getMonth() +1;
+                int year = dp.getYear();
+
+                tvDisplay.setText("Date is " + day + "/" + month + "/" + year);
+            }
+        });
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tp.setCurrentHour(0);
+                tp.setCurrentMinute(0);
+                dp.updateDate(2021,4,18);
+            }
+        });
+
+
+    }
+}
